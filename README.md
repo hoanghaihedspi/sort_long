a3e06959e6e4952962e753941e0e55ea8e4a8a1a
Hoàng Văn Hải <hoanghaihedspi@users.noreply.github.com>
2016-05-23 23:55:58 +0700

    Done
    fix sort.c, before version is wrong.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Input file "input.txt" have 1 million of signed 32-bit integers, separated by si
 Output file "output.txt" have 1 million of signed 32-bit integers, they were sorted by non decrease and separated by single spaces.
 
 # Run
-Running program in a Unix-like system.
+Run program in a Unix-like system.
 
 # Create input file "input.txt" with random data
 ```bash
@@ -14,10 +14,8 @@ $ gcc -w -o run creatTest.c -std=c11; ./run
 
 # Run program to sort data in file "input.txt" and write resuilt to file "output.txt"
 ```bash
-$ g++ -w -o run sort.cpp -std=c++11; ./run
+$ gcc -w -o run sort.c -std=c11; ./run
 ```
-# Memory
-Reducing the value of CUR\_MIN\_SIZE to reduce memory usage.
 
 # Image
-<img src="http://i.imgur.com/z6cwsA6.png?1">
+<img src="http://i.imgur.com/dKaheaB.png?1">	
--- sort.c
+++ sort.c
@@ -1,26 +1,34 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 #define SIZE 1000000	// Number of intergers
 
 void sort(char *file_in, char *file_out, long size);
-int long_compare(void const *a, void const *b);
+void merge_file(char *file_name1, long size1, char *file_name2, long size2, char *file_out);
+void remove_file(char *file_name);
+int long_compare(const void *a, const void *b);
 
 int main()
 {
 	sort("input.txt", "output.txt", SIZE);
+	// merge_file("1.txt", 3, "2.txt", 3, "output.txt");
 	return 0;
 }
 
 void sort(char *file_in, char *file_out, long size)
 {
 	long t = size / 2, tmp;
-	long *arr = (long*)malloc((t + 1) * sizeof(long));
+	long *arr = (long*)malloc((t + 10) * sizeof(long));
 
+	char *ftmp0 = "tmp0~.txt", *ftmp1 = "tmp1~.txt";
 	FILE *fin = fopen(file_in, "r");
-	FILE *fout = fopen(file_out, "w");
+	FILE *ftmp[2];
+	ftmp[0] = fopen(ftmp0, "w");
+	ftmp[1] = fopen(ftmp1, "w");
 
 	// Read data from file by 2 times
-	for(char times = 0; times < 2; times++){
+	for(int times = 0; times < 2; times++){
 		// Input the half of file data to array 'arr'
 		for(long i = 0; i < t; i++){
 			fscanf(fin, "%ld", &tmp);
@@ -29,17 +37,64 @@ void sort(char *file_in, char *file_out, long size)
 		// Sort array 'arr' by 'quick sort'
 		qsort(arr, t, sizeof(long), long_compare);
 		for(long i = 0; i < t; i++)
-			fprintf(fout, "%ld ", arr[i]);
+			fprintf(ftmp[times], "%ld ", arr[i]);
 
 		t = size - t;
 	}
 
 	free(arr);
 	fclose(fin);
-	fclose(fout);
+	fclose(ftmp[0]);
+	fclose(ftmp[1]);
+
+	merge_file(ftmp0, t, ftmp1, size - t, file_out);
 }
 
-int long_compare(void const *a, void const *b)
+void merge_file(char *file_name1, long size1, char *file_name2, long size2, char *file_out)
 {
-	return *(long*)a - *(long*)b;
+	FILE *f = fopen(file_out, "w");
+	FILE *f1 = fopen(file_name1, "r");
+	FILE *f2 = fopen(file_name2, "r");
+	if(!f || !f1 || !f2){ printf("Error open file!\n"); exit(1); }
+
+	long tmp1, tmp2;
+
+	fscanf(f1, "%ld", &tmp1);
+	fscanf(f2, "%ld", &tmp2);
+	while(size1 > 0 && size2 > 0){
+		if(tmp1 < tmp2){
+			fprintf(f, "%ld ", tmp1);
+			fscanf(f1, "%ld", &tmp1);
+			size1--;
+		}
+		else{
+			fprintf(f, "%ld ", tmp2);
+			fscanf(f2, "%ld", &tmp2);
+			size2--;
+		}
+	}
+	while(size1-- > 0){
+		fprintf(f, "%ld ", tmp1);
+		fscanf(f1, "%ld", &tmp1);
+	}
+	while(size2-- > 0){
+		fprintf(f, "%ld ", tmp2);
+		fscanf(f2, "%ld", &tmp2);
+	}
+	
+	fclose(f); fclose(f1); fclose(f2);
+	remove_file(file_name1);
+	remove_file(file_name2);
+}
+
+void remove_file(char *file_name)
+{
+	char tmp[20] = "rm ";
+	strcat(tmp, file_name);
+	system(tmp);
+}
+
+int long_compare(const void *a, const void *b)
+{
+	return (*(long*)a - *(long*)b);
 }
